6640f77d52eddb1324c40625b601344d2a882d92
Mansur <mansur.rizoev.m@gmail.com>
2024-08-27 16:52:20 +0500

    fixed

--- src/android/QrActivity.java
+++ src/android/QrActivity.java
@@ -50,6 +50,9 @@ import android.content.Context;
 import static android.Manifest.permission.READ_MEDIA_IMAGES;
 import static android.Manifest.permission.READ_MEDIA_VIDEO;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+
 public class QrActivity extends Activity implements ZXingScannerView.ResultHandler {
     private ZXingScannerView mScannerView;
     private static final int ZXING_CAMERA_PERMISSION = 1;
